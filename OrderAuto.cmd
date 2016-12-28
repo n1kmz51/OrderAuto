1681051bd618d73ace52c4001be3f83485adb8d7
n1kmz <n1kmz51@ya.ru>
2016-12-28 14:42:54 +0300

    OrderAuto.cmd set to v2

--- OrderAuto.cmd
+++ OrderAuto.cmd
@@ -1,41 +1,35 @@
-rename i:\lparfum\store.exe _store.exe
-rename i:\lparfum\plateg.exe _plateg.exe
-rename i:\lparfum\Qstat.exe _Qstat.exe
-rename i:\lparfum\stat.exe _stat.exe
-
+set lp=i:\lparfum
 set dat=i:\lparfum\dat
+set month_dir=000%date:~3,2%
+set backup=d:\save_tmp
+mkdir .\lparfum\dat\%month_dir%
+rename %lp%\store.exe _store.exe
+rename %lp%\plateg.exe _plateg.exe
+rename %lp%\Qstat.exe _Qstat.exe
+rename %lp%\stat.exe _stat.exe
+
+
 echo %date%-%time% > %dat%\upload.f
-copy i:\lparfum\dat\stor.* .\lparfum\dat /y /z
-copy i:\lparfum\dat\naklad.* .\lparfum\dat /y /z
-copy i:\lparfum\dat\00012\rec_buy.* .\lparfum\dat\00012 /y /z
+copy %dat%\stor.* .\lparfum\dat /y /z
+copy %dat%\naklad.* .\lparfum\dat /y /z
+copy %dat%\%month_dir%\rec_buy.* .\lparfum\dat\%month_dir% /y /z
 
 rem резервная копия rec_num
-copy i:\lparfum\dat\rec_num.* d:\save_tmp\recnum\ /y /z
-
-rem pause
+copy %dat%\rec_num.* %backup%\recnum\ /y /z
 
 d:\install\OrderAuto\OrderAuto.exe 4
-rem pause
 
-copy d:\install\OrderAuto\*.log d:\save_tmp\log\ /Y
+copy .\*.log %backup%\log\ /Y
 
 ping localhost -n 120
 
-xcopy .\lparfum\dat\00012 i:\lparfum\dat\00012 /E /Y /Z
-xcopy .\lparfum\dat\naklad.* i:\lparfum\dat /Y /Z
-xcopy .\lparfum\dat\stor.* i:\lparfum\dat /Y /Z
+xcopy .\lparfum\dat\%month_dir% %dat%\%month_dir% /E /Y /Z
+xcopy .\lparfum\dat\naklad.* %dat% /Y /Z
+xcopy .\lparfum\dat\stor.* %dat% /Y /Z
  
 del /q /f %dat%\upload.f
-rem del .\LPARFUM\DAT\00004\rec_buy.k0*
-rem .\LPARFUM\FILES.DEF\CFIL.EXE .\LPARFUM\FILES.DEF\rec_buy.CLA REC_BUY .\LPARFUM\DAT\00006\rec_buy.DAT .\LPARFUM\DAT\00006\rec_buy.DAT
-rem del .\LPARFUM\DAT\naklad.k0*
-rem .\LPARFUM\FILES.DEF\CFIL.EXE .\LPARFUM\FILES.DEF\naklad.CLA NAKLAD .\LPARFUM\DAT\NAKLAD.DAT .\LPARFUM\DAT\NAKLAD.DAT
-rem del .\lparfum\dat\stor.k0*
-rem .\lparfum\files.def\cfil.exe .\lparfum\files.def\stor.cla STOR .\lparfum\dat\stor.dat .\lparfum\dat\stor.dat
 
-
-
-rename i:\lparfum\_store.exe store.exe
-rename i:\lparfum\_plateg.exe plateg.exe
-rename i:\lparfum\_Qstat.exe Qstat.exe
-rename i:\lparfum\_stat.exe stat.exe
+rename %lp%\_store.exe store.exe
+rename %lp%\_plateg.exe plateg.exe
+rename %lp%\_Qstat.exe Qstat.exe
+rename %lp%\_stat.exe stat.exe
