5ebb069cc0c5d3147c715773a217906f6043b191
n1kmz <n1kmz51@ya.ru>
2016-12-29 11:50:40 +0300

    OrderAuto.cmd delete old logs

--- OrderAuto.cmd
+++ OrderAuto.cmd
@@ -2,6 +2,12 @@ set lp=i:\lparfum
 set dat=i:\lparfum\dat
 set month_dir=000%date:~3,2%
 set backup=d:\save_tmp
+
+
+rem удаление старых логов в понедельник
+
+
+
 mkdir .\lparfum\dat\%month_dir%
 rename %lp%\store.exe _store.exe
 rename %lp%\plateg.exe _plateg.exe
@@ -19,7 +25,12 @@ copy %dat%\rec_num.* %backup%\recnum\ /y /z
 
 d:\install\OrderAuto\OrderAuto.exe 4
 
+rem резервная копия логов
 copy .\*.log %backup%\log\ /Y
+
+rem удаление старых логов
+set skip = 5
+for /f "skip=5" %%i in ('dir /a:-d /o:-d /b *.log') do del %%i
 
 ping localhost -n 120
 
